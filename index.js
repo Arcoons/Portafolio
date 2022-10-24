bf2ed500c8189fd4a0277584a52c3502f154180c
Spike <christianzaor1090@gmail.com>
2022-10-24 10:01:42 -0500

    puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 'https://arcoons.github.io/Portafolio/cris/'
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs')
@@ -13,9 +13,6 @@ app.get('/clientes', (req, res) => {
 
 app.use('/cris', require('./routes/rutas'));
 
-  app.listen(port, () => {
-    console.log(`Conectado al puerto ${port}`)
-  })
   app.use((req,res,next)=>{
     res.status(404).sendFile(__dirname + '/public/404.html')
   })
